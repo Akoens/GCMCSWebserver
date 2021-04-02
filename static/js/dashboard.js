d0e3fe9f831bb6d4ab95f62007a09384a25cb94d
Arjan <arjan@koens.biz>
2021-04-02 17:41:54 +0200

    Database change location and ignore.

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -12,6 +12,8 @@ $(document).ready(function () {
         $("#ldr").text(data.data.ldr);
         $("#tmp").text(data.data.tmp);
         $("#dht").text(data.data.dht);
+        $("#ds20b18").text(data.data.ds20b18);
+
       },
     });
   }
